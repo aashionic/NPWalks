de63c164483d50e54a7f80ada265dc4082b366bc
Aashish Adhikari <pingaashish@gmail.com>
2023-08-12 14:22:13 +0545

    Registration

---
+++ NPWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using NPWalks.API.Models.DTO;
+
+namespace NPWalks.API.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> userManager;
+
+    public AuthController(UserManager<IdentityUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    //POST: /api/Auth/Register
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+    {
+        var identityUser = new IdentityUser
+        {
+            UserName = registerRequestDto.Username,
+            Email = registerRequestDto.Username
+        };
+
+        var identityResult = await userManager.CreateAsync(
+            identityUser,
+            registerRequestDto.Password
+        );
+        if (identityResult.Succeeded)
+        {
+            //Add roles to this User(reader/writer)
+            if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+            {
+                identityResult = await userManager.AddToRolesAsync(
+                    identityUser,
+                    registerRequestDto.Roles
+                );
+            }
+
+            if (identityResult.Succeeded)
+            {
+                return Ok("User was registered! Please Login.");
+            }
+        }
+
+        return BadRequest("Something went wrong");
+    }
+}
